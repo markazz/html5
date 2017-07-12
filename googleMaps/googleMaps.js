80dfc907aa89cbcf3ccd8744580be4dba3488f4e
markazz <azizconqueror1@gmail.com>
2017-07-12 19:56:28 +0100

    Website displaying Google Map

---
+++ googleMaps/googleMaps.js
@@ -0,0 +1,12 @@
+
+					function myMap(){
+var mapOptions = {
+	center : new google.maps.LatLng(51.5, -0.12),
+	zoom:10,
+	mapTypeId: google.maps.MapTypeID.HYBRID
+}//object
+
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+					}// myMap function
+
+				
